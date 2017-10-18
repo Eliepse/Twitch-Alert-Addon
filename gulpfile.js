eb8d7eeea89893127ade1d7385384271612c246c
Élie Meignan <contact@eliepse.fr>
2017-10-18 18:23:48 +0200

    Fix concat and add yargs

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var watch = require('gulp-watch');
+var argv = require('yargs').argv;
+
+var isProduction = (argv.production === undefined) ? false : true;
 
 gulp.task('make', ['concat-js'], function() {
 
@@ -14,11 +17,19 @@ gulp.task('make', ['concat-js'], function() {
 
 gulp.task('concat-js', function() {
 
-    return gulp.src([
-        'src/assets/js/reqwest.js',
-        'src/background_scripts/main.js',
-        'src/background_scripts/request.js',
-    ])
+    var src = [];
+
+    src.push('src/assets/js/reqwest.js')
+
+    if(isProduction)
+        src.push('src/config.js')
+    else
+        src.push('src/config.dev.js')
+
+    src.push('src/background_scripts/main.js')
+    src.push('src/background_scripts/request.js')
+
+    return gulp.src(src)
     .pipe(concat('app.js'))
     .pipe(gulp.dest('./addon/background_scripts/'))
 
@@ -26,6 +37,6 @@ gulp.task('concat-js', function() {
 
 gulp.task('watch', ['make'], function() {
 
-    return watch('src/**/*', ['make']);
+    return watch('./src/**/*.js', ['make']);
 
 })
